ba54f1c1a3988c027035692a615657e1350ce39c
Jashu1724 <jaswanth5757@gmail.com>
2023-12-11 15:15:53 +0530

    APP Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import APP from './Components/APP';
 function App() {
   return (
     <div className="App">
       <h1>React tutorial</h1>
+      <APP/>
     </div>
   );
 }
